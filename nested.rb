d3213df2f819d5f9eceb1617b9a1e61adaa59cd3
Senada Kadric <skadric@fordham.edu>
2020-04-23 23:09:49 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -63,10 +63,6 @@ end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-:yukihiro_matsumoto => {
-  :known_for => "Ruby",
-     :languages => ["LISP", "C"]
- }
 # return the entire updated hash
 
 	programmer_hash =
